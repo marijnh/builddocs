9afb4f4dc447b27032e563670dd9ca7e04353e5d
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-02 10:02:49 +0200

    Move to a system where there's a main template that lists all items
    + arbitrary text and doc structure.

--- src/builddocs.js
+++ src/builddocs.js
@@ -10,22 +10,35 @@ exports.browserImports = require("./browser")
 exports.build = function(config, data) {
   if (!data) data = read(config)
 
-  var cx = new Context(config, data)
-  return cx.mold.defs.module()
+  var placed = Object.create(null)
+  var doc = markdown.render(fs.readFileSync(config.main, "utf8").replace(/(^|\n)@(\w+)(?=$|\n)/g, function(_, before, name, after) {
+    if (placed[name]) throw new Error("Item " + name + " is included in doc template twice")
+    if (!data.items[name]) throw new Error("Unknown item " + name + " included in doc template")
+    placed[name] = true
+    return before + before + '<div data-item="' + name + '"></div>\n'
+  }))
+  for (var name in data.items) if (!placed[name])
+    throw new Error("Item " + name + " is missing from the doc template")
+
+  var mold = loadTemplates(config, data)
+
+  return doc.replace(/<div data-item="([^"]+)"><\/div>/g, function(_, name) {
+    let item = data.items[name]
+    return mold.defs.item({item: item, name: name})
+  })
 }
 
-function Context(config, data) {
-  this.config = config
-  this.prefix = config.anchorPrefix == null ? config.name + "." : config.anchorPrefix
-  this.data = data
-  this.mold = this.loadTemplates()
+function prefix(config) {
+  var prefix = config.anchorPrefix
+  if (prefix == null) prefix = config.name + "."
+  return prefix
 }
 
-Context.prototype.loadTemplates = function() {
-  var mold = new Mold(this.moldEnv()), conf = this.config
+function loadTemplates(config, data) {
+  var mold = new Mold(moldEnv(config, data))
   mold.defs.markdown = function(text) {
     if (!text) return ""
-    return markdown.render(conf.markdownFilter ? conf.markdownFilter(text) : text)
+    return markdown.render(config.markdownFilter ? config.markdownFilter(text) : text)
   }
   mold.defs.markdownFile = function(name) {
     return mold.defs.markdown(fs.readFileSync(name + ".md", "utf8"))
@@ -39,64 +52,48 @@ Context.prototype.loadTemplates = function() {
     })
   }
 
-  if (conf.templates) templateDir(conf.templates)
+  if (config.templates) templateDir(config.templates)
   templateDir(__dirname + "/../templates")
 
   return mold
 }
 
-function maybeLinkType(self, name) {
-  if (name in self.data.all) return "#" + self.prefix + name
+function maybeLinkType(config, data, name) {
+  if (name in data.all) return "#" + prefix(config) + name
   if (name.charAt(0) == '"') return false
-  var imports = self.config.imports, qualified = self.config.qualifiedImports
+  var imports = config.imports, qualified = config.qualifiedImports
   if (imports) for (var i = 0; i < imports.length; i++) {
     var set = imports[i]
     if (Object.prototype.hasOwnProperty.call(set, name))
       return set[name]
   }
-  if (qualified) for (var prefix in qualified) if (name.indexOf(prefix + ".") == 0) {
-    var inner = name.slice(prefix.length + 1)
-    if (Object.prototype.hasOwnProperty.call(qualified[prefix], inner))
-      return qualified[prefix][inner]
+  if (qualified) for (var pref in qualified) if (name.indexOf(pref + ".") == 0) {
+    var inner = name.slice(pref.length + 1)
+    if (Object.prototype.hasOwnProperty.call(qualified[pref], inner))
+      return qualified[pref][inner]
   }
   if (builtins.hasOwnProperty(name)) return builtins[name]
 }
 
-Context.prototype.moldEnv = function() {
-  var contentPos = 0
-
-  var self = this, env = {
-    moduleName: this.config.name,
-    prefix: this.prefix,
-    moduleText: this.data.extraText,
-    items: this.data.items,
-    pieces: this.data.pieces,
+function moldEnv(config, data) {
+  var env = {
+    prefix: prefix(config),
     linkType: function(type) {
-      var link = maybeLinkType(self, type.type)
-      if (!link && link !== false && !self.config.allowUnresolvedTypes)
+      var link = maybeLinkType(config, data, type.type)
+      if (!link && link !== false && !config.allowUnresolvedTypes)
         throw new Error("Unknown type '" + type.type + "' at " + type.loc.file + ":" + type.loc.line)
       return link
     },
-    anchor: function(item) {
-      return self.prefix + item.id
-    },
-    itemName: function(item) {
-      return /[^\.^]+$/.exec(item.id)[0]
-    },
     hasDescription: function(type) {
       if (type.description) return true
-      if (type.properties) for (let prop in type.properties)
+      if (type.properties) for (var prop in type.properties)
         if (env.hasDescription(type.properties[prop])) return true
-      if (type.params) for (let i = 0; i < type.params.length; i++)
+      if (type.params) for (var i = 0; i < type.params.length; i++)
         if (env.hasDescription(type.params[i])) return true
       if (type.returns && type.returns.description) return true
       return false
     }
   }
-  if (this.config.env) for (var prop in this.config.env) env[prop] = this.config.env[prop]
+  if (config.env) for (var prop in config.env) env[prop] = config.env[prop]
   return env
-}
-
-function notEmpty(obj) {
-  for (var _ in obj) return obj
 }
--- src/read.js
+++ src/read.js
@@ -3,60 +3,20 @@ var glob = require("glob")
 var getdocs = require("getdocs")
 
 exports.read = function(config) {
-  var items = Object.create(null), content = [], last
-  var files = filesFor(config)
-  files.forEach(function(filename) {
-    last = null
-    var file = fs.readFileSync(filename, "utf8")
-    getdocs.gather(file, {
-      filename: filename,
-      items: items,
-      onComment: function(block, text, _start, _end, startPos) {
-        var m
-        if (last && !block && startPos.line == last.loc.line + 1)
-          last.text += "\n" + text
-        else if (m = /^\s*!!(?:$|\s)/.exec(text))
-          content.push(last = {loc: {line: startPos.line, file: filename}, text: text.slice(m[0].length)})
-      }
-    })
-  })
-
-  function isBefore(element, item) {
-    var fDiff = files.indexOf(element.loc.file) - files.indexOf(item.loc.file)
-    if (fDiff < 0) return true
-    return fDiff == 0 && element.loc.line < item.loc.line
-  }
-
-  let pieces = [], i = 0
-  for (let name in items) {
-    let item = items[name]
-    while (i < content.length && isBefore(content[i], item))
-      pieces.push({content: getdocs.stripComment(content[i++].text)})
-    pieces.push(item)
-  }
-  while (i < content.length)
-    pieces.push({content: getdocs.stripComment(content[i++].text)})
-
-  return {
-    items: items,
-    pieces: pieces,
-    all: gatherAll({properties: items}, Object.create(null))
-  }
-}
-
-function filesFor(config) {
+  var items = Object.create(null)
   var files = config.files.split(" ").reduce(function(set, pat) {
     return set.concat(glob.sync(pat))
   }, [])
-  if (!config.order) return files
-
-  var ordered = config.order.split(" ").map(function(pat) {
-    var re = new RegExp("\\/" + pat + "\\.\\w+$")
-    for (var i = 0; i < files.length; i++)
-      if (files[i].match(re)) return files.splice(i, 1)[0]
-    throw new Error("Order pattern " + pat + " does not match a file")
+  files.forEach(function(filename) {
+    last = null
+    var file = fs.readFileSync(filename, "utf8")
+    getdocs.gather(file, {filename: filename, items: items})
   })
-  return ordered.concat(files)
+
+  return {
+    items: items,
+    all: gatherAll({properties: items}, Object.create(null))
+  }
 }
 
 function gatherAll(obj, target) {
