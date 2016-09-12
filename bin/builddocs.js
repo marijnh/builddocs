6bdc3d78534b8b08cfe53d0630c80c1301f684e3
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-12 16:19:47 +0200

    Make the bin/ file actually work with current code

--- bin/builddocs.js
+++ bin/builddocs.js
@@ -2,7 +2,29 @@ var fs = require("fs")
 
 var build = require("../src/builddocs").build
 
-var config = JSON.parse(fs.readFileSync(".builddocs", "utf8"))
-if (!config.name) config.name = JSON.parse(fs.readFileSync("package.json", "utf8")).name
+var name, main, templates, allowUnresolved = true, files = []
 
-console.log(build(config))
+function help(status) {
+  console.log("Usage: builddocs --name <name> --main <main> [--templates <templatedir>]\n         [--disallow-unresolved] [--help] <sourcefiles>")
+  process.exit(status)
+}
+
+for (var i = 2; i < process.argv.length; i++) {
+  var arg = process.argv[i]
+  if (arg == "--name") name = process.argv[++i]
+  else if (arg == "--main") main = process.argv[++i]
+  else if (arg == "--templates") templates = process.argv[++i]
+  else if (arg == "--disallow-unresolved") allowUnresolved = false
+  else if (arg.charAt(0) != "-") files.push(arg)
+  else help(arg == "--help" ? 0 : 1)
+}
+
+if (!main || !name || !files.length) help(1)
+
+console.log(build({
+  name: name,
+  main: main,
+  files: files.join(" "),
+  templates: templates,
+  allowUnresolvedTypes: allowUnresolved
+}))
